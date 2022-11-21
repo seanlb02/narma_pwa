52091ccdad262ed2182e9ec41e6e27dd83a4ede4
Sean <12708@coderacademy.edu.au>
2022-11-21 21:15:28 +1100

    completed basic player

---
+++ ctwebsite/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Homebody from '../components/Homebody'
+
+export default function Home() {
+  return (
+    
+    <Homebody/>
+  )
+}
